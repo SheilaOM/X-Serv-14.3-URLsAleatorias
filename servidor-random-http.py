1484883b2f95803f7f9ced008149c083916ae4a3
SheilaOM <sheila.oliva14@gmail.com>
2017-02-08 17:29:28 +0100

    Version acabada

--- servidor-random-http.py
+++ servidor-random-http.py
@@ -1,31 +1,16 @@
 #!/usr/bin/python
 
 """
-Simple HTTP Server
-Jesus M. Gonzalez-Barahona and Gregorio Robles
-{jgb, grex} @ gsyc.es
-TSAI, SAT and SARO subjects (Universidad Rey Juan Carlos)
+Seila Oliva Muñoz
 """
 
 import socket
 import random
 
-# Create a TCP objet socket and bind it to a port
-# We bind to 'localhost', therefore only accepts connections from the
-# same machine
-# Port should be 80, but since it needs root privileges,
-# let's use one above 1024
-
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySocket.bind(('localhost', 1234))
 
-# Queue a maximum of 5 TCP connection requests
-
 mySocket.listen(5)
-
-# Accept connections, read incoming data, and answer back an HTML page
-#  (in an infinite loop)
-
 
 while True:
     a = random.randint(0, 1000000000)
